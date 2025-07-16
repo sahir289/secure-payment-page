fa3a801e9c7f997e42684a02f8ce153c9b7aca31
Luv-83 <luvraghuvanshi83@gmail.com>
2025-07-16 13:50:59 +0400

    add base url

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,10 +1,8 @@
 export const API_CONFIG = {
-//   BASE_URL: 'https://api.staging.trustpays24.com/v1',
-BASE_URL: 'http://localhost:8090/v1', // Adjust this to your actual API base URL
-  
+  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   ENDPOINTS: {
     GENERATE_PAYIN: '/payIn/generate-payin',
-    VALIDATE_TOKEN: '/payIn/validate-payIn-url',  // Assuming this is your validation endpoint
+    VALIDATE_TOKEN: '/payIn/validate-payIn-url',
     ASSIGN_BANK: '/payIn/assign-bank',
     PROCESS_TRANSACTION: '/payIn/process',
     IMAGE_SUBMIT: '/payIn/image-submit'
